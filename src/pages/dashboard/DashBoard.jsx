acf8f3b007e778f085abb706d49c8323199c103f
Non <non.iti.19@gmail.com>
2024-09-12 01:34:31 +0700

    dashboard

--- src/pages/dashboard/DashBoard.jsx
+++ src/pages/dashboard/DashBoard.jsx
@@ -2,7 +2,8 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, {useEffect, useMemo, useState} from 'react'; 
 import CountUp from 'react-countup'; 
-import { Divider, Card, List, Flex, Row, Space, Typography , Table } from 'antd';
+import { Divider, Card, List, Flex, Row, Space, Typography ,message, Table, Form, Col } from 'antd';
+import { Input, Button } from "antd";
 
 import DashBoardService from '../../service/DashBoard.service';
 
@@ -21,20 +22,20 @@ const pagging = { pagination: { current: 1, pageSize: 10, }, };
 const dsbservice = DashBoardService();
 function DashBoard() {
     const [selectedMenu, setSelectedMenu] = useState(null);
-    
+    const [listdata, setListData] = useState([]);
+
     const hendelOpenMenu = async (value)=>{ 
       console.log(value)
-      try {
-        // เรียกข้อมูลจาก backend
-      const res = await dsbservice.getReport({ menu: value }).catch((error) => message.error("get report data fail."));
-      const { data } = res.data; 
-      setListStudent(null);
-      setListStudent(data);
-      message.success('ดึงข้อมูลสำเร็จ');
-      } catch (error) {
-        console.error('Error fetching data:', error);
-        message.error('มีบางอย่างผิดพลาด กรุณาลองใหม่อีกครั้ง');
-      }
+      // try {
+      //   // เรียกข้อมูลจาก backend
+      // const res = await dsbservice.getReport({ menu: value }).catch((error) => message.error("get report data fail."));
+      // const { data } = res.data; 
+      // setListData(data);
+      // message.success('ดึงข้อมูลสำเร็จ');
+      // } catch (error) {
+      //   console.error('Error fetching data:', error);
+      //   message.error('มีบางอย่างผิดพลาด กรุณาลองใหม่อีกครั้ง');
+      // }
       setSelectedMenu(value);
     }
 
@@ -65,7 +66,56 @@ function DashBoard() {
             {!selectedMenu && ListMenu} 
             {selectedMenu && (
               <div style={{ marginTop: '50px' }}>
-                {selectedMenu} 
+                <Form  layout="vertical" autoComplete="off">
+                <Row gutter={[8, 8]}>
+                <Col xs={24} sm={6} md={6} lg={6} xl={6}>
+                    <Form.Item
+                      label="ชื่อวิชา"
+                      name="subject_name"
+                      //onChange={handleSearch}
+                    >
+                      <Input placeholder="กรอกชื่อวิชา" />
+                    </Form.Item>
+                  </Col>
+                  <Col xs={24} sm={6} md={6} lg={6} xl={6}>
+                    <Form.Item
+                      label="รายละเอียดวิชา"
+                      name="description"
+                      //onChange={handleSearch}
+                    >
+                      <Input placeholder="กรอกรายละเอียดวิชา" />
+                    </Form.Item>
+                  </Col>
+                </Row>
+                <Row gutter={[8, 8]}>
+                  <Col xs={24} sm={8} md={12} lg={12} xl={12}>
+                    {/* Ignore */}
+                  </Col>
+                  <Col xs={24} sm={8} md={12} lg={12} xl={12}>
+                    <Flex justify="flex-end" gap={8}>
+                      <Button
+                        type="primary"
+                        size="small"
+                        className="bn-action"
+                        icon={<SearchOutlined />}
+                        onClick={() => handleSearch()}
+                      >
+                        ค้นหา
+                      </Button>
+                      <Button
+                        type="primary"
+                        size="small"
+                        className="bn-action"
+                        danger
+                        icon={<ClearOutlined />}
+                        onClick={() => handleClear()}
+                      >
+                        ล้าง
+                      </Button>
+                    </Flex>
+                  </Col>
+                </Row>
+              </Form>
               </div>
             )}
             </div> 
--- src/service/DashBoard.service.js
+++ src/service/DashBoard.service.js
@@ -1,27 +1,14 @@
 import { requestService as api } from "./Request.service"  
 const API_URL = { 
-  API_SAMPLELIST: `/dashboard/get-sample-list.php`,  
-  API_SAMPLEREQUEST_LIST: `/dashboard/get-sample-request-list.php`,  
-  API_SAMPLEREQUEST_DETAIL: `/dashboard/get-sample-request-details.php`,  
-  API_FILEEXPIRE: `/dashboard/get-items-files-expiry.php`,  
-  API_STATISTICS: `/dashboard/get-statistic-value.php`,  
+  API_GETREPORT: `/dashboard/get-report.php`,  
 };
   
 const DashBoardService = () => { 
   
-  const samplelist = (parm = {}, load = false) => api.post(`${API_URL.API_SAMPLELIST}`, parm, { ignoreLoading : load });
-  const sample_requestlist = (parm = {}, load = false) => api.post(`${API_URL.API_SAMPLEREQUEST_LIST}`, parm, { ignoreLoading : load });
-  const sample_requestdetail = (parm = {}, load = false) => api.post(`${API_URL.API_SAMPLEREQUEST_DETAIL}`, parm, { ignoreLoading : load });
-  const filesexpire = (parm = {}, load = false) => api.post(`${API_URL.API_FILEEXPIRE}`, parm, { ignoreLoading : load });
-  const statistics = (load = false) => api.post(`${API_URL.API_STATISTICS}`, null, { ignoreLoading : load });
+  const getReport = (parm = {}) => api.post(`${API_URL.API_GETREPORT}`, parm);
 
   return {
-    samplelist,
-    sample_requestlist,
-    sample_requestdetail,
-
-    filesexpire,
-    statistics,
+    getReport,
 
   };
 };
